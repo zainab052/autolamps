cb93ec1eb4d7d71ea380b8efeb5267417e74a13a
zainab052 <32038609+zainab052@users.noreply.github.com>
2023-06-08 14:49:48 +0300

    Delete projects/autolamps-copy/parts directory

--- projects/autolamps-copy/parts/account-financial-reporting/account_financial_report_webkit_xls/__init__.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-try:
-    from . import wizard
-    from . import report
-except ImportError:
-    import logging
-    logging.getLogger('openerp.module').warning('''report_xls not available in
-    addons path. account_financial_report_webkit_xls will not be usable''')
